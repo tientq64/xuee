37ba58e5dac41cb30730e1e7b6905966d8955372
tientq64 <tientq64@gmail.com>
2025-05-14 13:39:23 +0700

    chore: đổi release.config sang module

---
+++ release.config.mjs
@@ -0,0 +1,28 @@
+import conventionalChangelogEmoji from '@favoloso/conventional-changelog-emoji'
+
+export const branches = ['main']
+export const plugins = [
+    [
+        '@semantic-release/commit-analyzer',
+        {
+            preset: 'conventionalcommits'
+        }
+    ],
+    [
+        '@semantic-release/release-notes-generator',
+        {
+            writerOpts: {
+                transform: conventionalChangelogEmoji
+            }
+        }
+    ],
+    '@semantic-release/changelog',
+    [
+        '@semantic-release/git',
+        {
+            assets: ['CHANGELOG.md', 'package.json'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+    ],
+    '@semantic-release/github'
+]
